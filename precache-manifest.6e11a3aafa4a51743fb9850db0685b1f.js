c37e8db6fe1eac7302878071dd9abe0afd4adafc
Kutabix <bartekd123456@wp.pl>
2020-05-16 03:16:13 +0200

    Updates

---
+++ precache-manifest.6e11a3aafa4a51743fb9850db0685b1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2c04514ac0e5eae200e4754837447b",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "b41dbab7bbeadc21f789",
+    "url": "/corona-tracker/static/js/2.e5c035da.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/corona-tracker/static/js/2.e5c035da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e8c1c2311dd6fbf655",
+    "url": "/corona-tracker/static/js/main.98816635.chunk.js"
+  },
+  {
+    "revision": "40acf82501ce4de14340",
+    "url": "/corona-tracker/static/js/runtime-main.8f6be270.js"
+  },
+  {
+    "revision": "e578d687f0d03af898ea43f2c622bfab",
+    "url": "/corona-tracker/static/media/cov-logo.e578d687.png"
+  }
+]);
